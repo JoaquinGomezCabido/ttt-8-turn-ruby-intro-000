f7aad1289854c9d6df108068d0b127c2c381c76b
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-12 19:51:03 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if(index.between?(0,8))
+  if(index.between?(0,9))
     if(!position_taken?(board, index))
       return true
     end
