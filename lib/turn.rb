2a928ef213b69753c023c6bc1178f6e5b0fe2987
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-12 19:56:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,7 +34,8 @@ end
 
 def turn (board)
   puts "Please enter 1-9: "
-  index = input_to_index(gets.to_i)
+  input = gets.to_i
+  index = input_to_index(input)
   if (valid_move(board, index))
     move(board, index, symbol)
     display_board(board)
